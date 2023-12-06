c6381c9c27e627997e6ef6220ca25827d4ec6f14
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-06 10:57:34 +0200

    feat: Initial configuration for swagger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { initializeDB } from './helpers/db';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   await initializeDB();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const options = new DocumentBuilder()
+    .setTitle('Todo API')
+    .setDescription('A simple Todo API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
