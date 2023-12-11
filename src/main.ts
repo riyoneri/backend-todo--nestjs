0c0346d51b4f41bc52de21a80892cebb03beb831
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-11 13:10:15 +0200

    feat: add default categories and tasks

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+import { JsonDB, Config } from 'node-json-db';
+
+export const db = new JsonDB(new Config('todos-db', true, true, '/'));
+db.push('/categories', []);
+db.push('/tasks', []);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
