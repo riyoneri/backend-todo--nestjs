869a4ab4ec28b118ac991a84ed9f959646d415f0
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-11 13:17:34 +0200

    feat: get all categories repository

--- src/categories/categories.repository.ts
+++ src/categories/categories.repository.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InternalServerErrorException } from '@nestjs/common';
+import { db } from 'src/main';
+import { customAlphabet } from 'nanoid';
+import { Category } from './category.entity';
+
+const alphabet = '123456789';
+const nanoid = customAlphabet(alphabet, 3);
 
 @Injectable()
 export class CategoriesRepository {
-  getAllCategories() {
-    return 'getting all categories';
+ 
+
+  async getAllCategories() {
+    try {
+      const allCategories = await db.getData('/categories');
+      return allCategories;
+    } catch (err) {
+      throw new InternalServerErrorException(
+        'An error occured while fetching categories',
+      );
+    }
   }
 }
