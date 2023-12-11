3c879477662af654f899fb513624eea80544406e
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-11 17:45:06 +0200

    feat: add all modules in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TasksService } from './tasks/tasks.service';
-import { CategoriesService } from './categories/categories.service';
-import { CategoriesController } from './categories/categories.controller';
-import { CategoriesRepository } from './categories/categories.repository';
-import { TasksRepository } from './tasks/task.repository';
-import { TasksController } from './tasks/tasks.controller';
+import { CategoriesModule } from './categories/categories.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  controllers: [CategoriesController, TasksController],
-  providers: [
-    TasksService,
-    CategoriesService,
-    CategoriesRepository,
-    TasksRepository,
-  ],
+  imports: [CategoriesModule, TasksModule],
 })
 export class AppModule {}
