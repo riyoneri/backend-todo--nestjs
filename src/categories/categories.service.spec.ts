7c019c88d19cf0340cc4ac9d67046fa151c49195
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-12 18:53:14 +0200

    feat: categories service unit testing

--- src/categories/categories.service.spec.ts
+++ src/categories/categories.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesService } from './categories.service';
+import { CategoriesRepository } from './categories.repository';
+import { Category } from './category.entity';
 
 describe('CategoriesService', () => {
   let service: CategoriesService;
+  const fakeCategoriesRepository: Partial<CategoriesRepository> = {
+    addCategory: () => Promise.resolve(),
+    getAllCategories: (): Promise<Category[]> =>
+      Promise.resolve([{ id: '1', name: 'Work' }]),
+    deleteCategory: () => Promise.resolve(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoriesService],
+      providers: [
+        CategoriesService,
+        { provide: CategoriesRepository, useValue: fakeCategoriesRepository },
+      ],
     }).compile();
 
     service = module.get<CategoriesService>(CategoriesService);
@@ -14,5 +25,21 @@ describe('CategoriesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create category', async () => {
+    const addedCategory = await service.createCategory('Work');
+    expect(addedCategory).toBeUndefined();
+  });
+
+  it('should get all categories', async () => {
+    const allCategories = await service.getAllCategories();
+
+    expect(allCategories).toBeInstanceOf(Array);
+  });
+
+  it('should delete category', async () => {
+    const deletedCategory = await service.deleteCategory('1');
+    expect(deletedCategory).toBeUndefined();
   });
 });
