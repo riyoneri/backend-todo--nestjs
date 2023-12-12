d175993be9d8f1f3e6a18672f707b0f6f1a2887e
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-12 14:41:10 +0200

    feat: delete category

--- src/categories/categories.repository.ts
+++ src/categories/categories.repository.ts
@@ -1,7 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  HttpException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InternalServerErrorException } from '@nestjs/common';
 import { db } from 'src/helpers/db';
 import { v4 as uuid } from 'uuid';
+import { Category } from './category.entity';
+import { Task } from 'src/tasks/task.entity';
 
 @Injectable()
 export class CategoriesRepository {
@@ -16,6 +23,34 @@ export class CategoriesRepository {
       throw new InternalServerErrorException(
         'An error occured while fetching categories',
       );
+    }
+  }
+
+  async deleteCategory(id: string) {
+    try {
+      const category: Category = await db.find(
+        '/categories',
+        (category: Category) => category.id === id,
+      );
+
+      if (!category) throw new NotFoundException('Category not found');
+
+      const categoryTask = await db.find(
+        '/tasks',
+        (task: Task) => task.category === category.id,
+      );
+
+      if (categoryTask)
+        throw new ConflictException(
+          'Category is being used by one or more tasks',
+        );
+
+      const categoryIndex = await db.getIndex('/categories', category.id);
+
+      return await db.delete(`/categories[${categoryIndex}]`);
+    } catch (err) {
+      if (err instanceof HttpException) throw err;
+      throw new InternalServerErrorException('Something went wrong!');
     }
   }
 }
