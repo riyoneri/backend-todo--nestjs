0cd2a5e2758cee19611749256032b73ed2e516a1
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-12 11:27:00 +0200

    feat: create swagger createCategoryDto

--- src/categories/dtos/create-category.dto.ts
+++ src/categories/dtos/create-category.dto.ts
@@ -1,6 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateCategoryDto {
+  @ApiProperty({
+    description: 'Name of new category to be added',
+  })
   @IsString()
   name: string;
 }
