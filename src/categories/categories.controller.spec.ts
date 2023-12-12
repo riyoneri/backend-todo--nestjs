b37b4ac14c3d7dbd1e79725c139000702d1a2e74
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-12 19:22:27 +0200

    feat: categories controllers test cases

--- src/categories/categories.controller.spec.ts
+++ src/categories/categories.controller.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from './categories.controller';
+import { CategoriesService } from './categories.service';
 
 describe('CategoriesController', () => {
   let controller: CategoriesController;
+  const fakeController: Partial<CategoriesService> = {
+    createCategory: () => Promise.resolve(),
+    getAllCategories: () => Promise.resolve([]),
+    deleteCategory: () => Promise.resolve(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CategoriesController],
+      providers: [{ provide: CategoriesService, useValue: fakeController }],
     }).compile();
 
     controller = module.get<CategoriesController>(CategoriesController);
@@ -14,5 +21,23 @@ describe('CategoriesController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create category', async () => {
+    const createdCategory = await controller.createCategory({ name: 'Work' });
+
+    expect(createdCategory).toBeUndefined();
+  });
+
+  it('should get all categories', async () => {
+    const allCategories = await controller.getAllCategories();
+
+    expect(allCategories).toBeInstanceOf(Array);
+  });
+
+  it('should delete category', async () => {
+    const deletedCategory = await controller.deleteCategory('2');
+
+    expect(deletedCategory).toBeUndefined();
   });
 });
