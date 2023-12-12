d829f0a28de7128f3a58b4cb7256859a85fa2a02
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-12 12:09:11 +0200

    feat: define api response for categories controller

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dtos/create-category.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Categories')
 @Controller('categories')
@@ -9,11 +9,31 @@ export class CategoriesController {
   constructor(private categoriesService: CategoriesService) {}
 
   @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'The category has been successfully created.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Missing body configurations',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error',
+  })
   createCategory(@Body() body: CreateCategoryDto) {
     return this.categoriesService.createCategory(body.name);
   }
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'All categories',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error',
+  })
   getAllCategories() {
     return this.categoriesService.getAllCategories();
   }
