6b922a143890fe9fcc0ee1d2b3b53cace9456007
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-12 11:27:29 +0200

    feat: create swagger createTaskDto

--- src/tasks/dtos/create-task.dto.ts
+++ src/tasks/dtos/create-task.dto.ts
@@ -1,12 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsUUID } from 'class-validator';
 
 export class CreateTaskDto {
+  @ApiProperty({
+    description: 'Title of new task',
+  })
   @IsString()
   title: string;
 
+  @ApiProperty({
+    description: 'Description of new task',
+  })
   @IsString()
   description: string;
 
+  @ApiProperty({
+    description: 'Id of category in which task belongs to(uuid)',
+    format: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+    example: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+  })
   @IsUUID()
   category: string;
 }
