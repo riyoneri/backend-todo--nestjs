62d47d9f0579e9dd863a669a41b92ffcc5167c06
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-13 20:57:20 +0200

    feat: refactoring tasks service

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,66 +1,39 @@
-import {
-  ForbiddenException,
-  Injectable,
-  NotFoundException,
-  HttpException,
-} from '@nestjs/common';
-import { InternalServerErrorException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { db } from '../helpers/db';
-import { Status, Task } from './task.entity';
+import { Task } from './task.entity';
 import { CreateTaskDto } from './dtos/create-task.dto';
-import { v4 as uuid } from 'uuid';
 import { Category } from 'src/categories/category.entity';
 
 @Injectable()
 export class TasksRepository {
   async getAllTasks() {
-    try {
-      return await db.getData('/tasks');
-    } catch (err) {
-      throw new InternalServerErrorException(
-        'An error occured while fetching tasks',
-      );
-    }
+    return await db.getData('/tasks');
   }
 
-  async getOneTask(id: string) {
-    try {
-      const task = await db.find('/tasks', (task: Task) => task.id === id);
-
-      if (!task) throw new NotFoundException('Task not found');
-
-      return task;
-    } catch (err) {
-      if (err instanceof HttpException) throw err;
-      throw new InternalServerErrorException('Something went wrong!');
-    }
+  async findOne(id: string) {
+    return await db.find('/tasks', (task: Task) => task.id === id);
   }
 
-  async createtask(body: CreateTaskDto) {
-    try {
-      const category = await db.find(
-        '/categories',
-        (category: Category) => category.id === body.category,
-      );
-
-      if (!category) throw new ForbiddenException('Invalid category id');
-
-      db.push('/tasks[]', { id: uuid(), ...body, status: Status.OPEN }, true);
-    } catch (err) {
-      if (err instanceof HttpException) throw err;
-      throw new InternalServerErrorException('Something went wrong!');
-    }
+  async findCategory(id: string) {
+    return await db.find(
+      '/categories',
+      (category: Category) => category.id === id,
+    );
   }
 
-  async deleteTask(id: string) {
-    try {
-      const taskIndex = await db.getIndex('/tasks', id, 'id');
-      if (taskIndex < 0) throw new NotFoundException('Task not found');
+  async createCategory(category: Category) {
+    return await db.push('/tasks[]', category, true);
+  }
 
-      return await db.delete(`/tasks[${taskIndex}]`);
-    } catch (err) {
-      if (err instanceof HttpException) throw err;
-      throw new InternalServerErrorException('Something went wrong!');
-    }
+  async createtask(task: CreateTaskDto) {
+    db.push('/tasks[]', task, true);
+  }
+
+  async getTaskIndex(id: string) {
+    return await db.getIndex('/tasks', id);
+  }
+
+  async deleteTask(index: number) {
+    return await db.delete(`/tasks[${index}]`);
   }
 }
